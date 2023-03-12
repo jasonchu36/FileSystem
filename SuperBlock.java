80606b4bb31b6f22f51ea2a175cd589ee807e394
Mohammad Zahid <adyanzahid786@gmail.com>
2023-03-12 16:30:47 -0700

    file structure

--- SuperBlock.java
+++ SuperBlock.java
@@ -18,6 +18,7 @@ class Superblock {
         
         if (totalBlocks == diskSize && totalInodes > 0  && freeList >= 2) {
             //return;
+            
         } else {
             totalBlocks = diskSize;
             format(defaultInodeBlocks);
