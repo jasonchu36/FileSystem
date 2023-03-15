019e5c6ad5cf10d5e79ee06b032e8c0710148390
jasonchu36 <jason.chu9743@gmail.com>
2023-03-14 17:53:15 -0700

    poopoo

--- FileSystem.java
+++ FileSystem.java
@@ -1,10 +1,32 @@
 public class FileSystem {
     private Vector table; // the actual entity of this file table
     private Directory dir; // the root directory
+    private SuperBlock superblock;
+    private FileTable filetable;
+
+    public FileSystem(int diskBlocks) {
+        superblock = new SuperBlock(diskBlocks);
+        dir = new Directory(superblock.totalInodes);
+        filetable = new FileTable(dir);
+
+        // read the "/" file from disk
+        FileTableEntry dirEnt = open("/", "r");
+        int dirSize = fsize(dirEnt);
+
+        if (dirSize > 0) {
+            // directory has some data
+            byte[] dirData = new byte[dirSize];
+            read(dirEnt, dirData);
+            dir.bytes2directory(dirData);
+        }
+        close(dirEnt);
+    }
+    
     public FileTable( Directory directory ) { // constructor
         table = new Vector( ); // instantiate a file (structure) table
         dir = directory; // receive a reference to the Director
     } // from the file system
+    
     // major public methods
     public synchronized FileTableEntry falloc( String filename, String mode ) {
         // allocate a new file (structure) table entry for this file name
@@ -21,7 +43,11 @@ public class FileSystem {
                 if (mode.compareTo("r") == 0) {
                     if (inode.flag == 0) { // == read. nothing needs to happen
                         break;
-                    } else if (inode.flag == ) // to be deleted
+                    } else if (inode.flag == ) // to be deleted 
+                    {
+                        iNumber = -1;
+                        return null;
+                    }
                     {
                         try{
                             wait();
@@ -52,4 +78,52 @@ public class FileSystem {
         // return if table is empty
         return table.isEmpty( ); 
     } // should be called before starting a format
+
+    public int read(FileTableEntry ftEnt, byte[] buffer) {
+        if (ftEnt.mode.compareTo("w") == 0 || ftEnt.mode.compareTo("a") == 0) {
+            return -1;
+        }
+        
+    }
+
+    public int write(FileTableEntry ftEnt, byte[] buffer) {
+
+    }
+
+    public int fsize(FileTableEntry ftEnt) {
+        assert (ftEnt != null);
+        return ftEnt.inode.length;
+    }
+
+    public boolean format (int files) {
+        this.superblock.format(files);
+        this.dir = new Directory(this.superblock.totalInodes);
+        this.filetable = new FileTable(this.dir);
+        return true;
+    }
+
+    public FileTableEntry open(String filename, String mode) {
+        FileTableEntry ftEnt = this.filetable.falloc(filename, mode);
+        if (ftEnt == null) {
+            return null;
+        }
+        if (mode.compareTo("w") == 0) {
+            if (this.deallocAllBlocks(ftEnt) == false) {
+                return null;
+            }
+        }
+        return ftEnt;
+    }
+
+    public boolean close(FileTableEntry ftEnt) {
+        synchronized (ftEnt) {
+            ftEnt.count--;
+            if (ftEnt.count > 0) {
+                return true;
+            }
+        }
+        return this.filetable.ffree(ftEnt);
+    }
+
+
 }
